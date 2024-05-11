70d585e65d5d9a29f01cc870b57c050a4a6ee989
nadia1624 <nadyadearihanifah@gmail.com>
2024-05-11 14:18:43 +0700

    ...

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
   res.render('login')
 });
 
